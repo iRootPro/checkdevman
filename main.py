2cfd2a51f8f6815a7aeb141490081746e60ddbcd
Alexander Neupokoev <admin@armavir.ru>
2020-01-25 20:12:54 +0300

    add chat_id to .env file

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from tg import  create_telegram_bot
 def main():
     TOKEN_DEVMAN = os.getenv('TOKEN_DEVMAN')
     TOKEN_TELEGRAM = os.getenv('TOKEN_TELEGRAM')
+    CHATID_TELEGRAM = os.getenv('CHATID_TELEGRAM')
+
     message_for_lesson, lesson_fails = check(TOKEN_DEVMAN)
-    create_telegram_bot(TOKEN_TELEGRAM, message_for_lesson, lesson_fails)
+    create_telegram_bot(TOKEN_TELEGRAM, CHATID_TELEGRAM, message_for_lesson, lesson_fails)
 
 if __name__ == '__main__':
     load_dotenv()
--- tg.py
+++ tg.py
@@ -1,11 +1,11 @@
 import telegram
 
 
-def create_telegram_bot(token_telegram, message_for_lesson, lesson_fails):
+def create_telegram_bot(token_telegram, chat_id, message_for_lesson, lesson_fails):
     if lesson_fails:
         message = f'{message_for_lesson}. К сожалению в работе нашли ошибки.'
     else:
         message =  f'{message_for_lesson}. Преподаватель принял работу.'
     bot = telegram.Bot(token=token_telegram)
 
-    bot.send_message(chat_id=34979247, text=message)
+    bot.send_message(chat_id=chat_id, text=message)
