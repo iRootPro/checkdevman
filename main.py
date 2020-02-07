672a10477112f1c0e62c01820b3b4ee5c7534a71
Alexander Neupokoev <admin@armavir.ru>
2020-02-07 20:44:01 +0300

    added logger.exception

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from settings import logger_config
 from check import check
 
 
-logging.config.dictConfig(logger_config)
 devman_logger = logging.getLogger('devman_logger')
 
 
 def main():
-    DEVMAN_TOKEN = os.getenv('DEVMAN_TOKEN')
+    logging.config.dictConfig(logger_config)
+    DEVMAN_TOKE = os.getenv('DEVMAN_TOKEN')
     TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
     TELEGRAM_CHATID = os.getenv('TELEGRAM_CHATID')
 
     devman_logger.debug('Бот запущен')
     try:
-    	check(DEVMAN_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHATID)
-    except Exception as error:
-    	devman_logger.error(error, exc_info=True)
+        check(DEVMAN_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHATID)
+    except Exception:
+        devman_logger.exception('Произошла ошибка')
+
 
 if __name__ == '__main__':
     load_dotenv()
     main()
-
